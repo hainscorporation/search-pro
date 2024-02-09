16c6b75b793b3bfc721fd846caa74276b0f71cc0
Daniela Cordoba <github@hains.au>
2024-02-09 12:19:23 +1000

    Customizind MUI theme - new font family Nunito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import OrdersTable from './components/OrdersTable/OrdersTable'
 
+const theme = createTheme({
+  pallete: {
+    text: {
+      primary: 'rgb(93, 108, 116)'
+    }
+  },
+  typography: {
+    fontFamily: [
+      'Nunito',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 function App() {
-  return <OrdersTable /> 
+  return (
+    <ThemeProvider theme={theme}>
+        <div className="App">
+          <OrdersTable /> 
+        </div>
+      </ThemeProvider>
+  )
 }
 
 export default App;
