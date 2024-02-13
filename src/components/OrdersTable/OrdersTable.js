ed095b6b70c700a0f414baa9287a178faa2790e4
Daniela Cordoba <dacorac43@gmail.com>
2024-02-13 17:14:22 +1000

    Customizing table sorting and pagination (client only)

--- src/components/OrdersTable/OrdersTable.js
+++ src/components/OrdersTable/OrdersTable.js
@@ -4,29 +4,52 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
 
 import OrderModal from '../OrderModal/OrderModal';
+import SortingTableHead from './SortingTableHead/SortingTableHead';
 
 import OrderReducer from "../../reducers/OrderReducer"
 import { dateFormatter } from '../../utils/formatDates';
+import { stableSort, getComparator } from '../../utils/sort';
 
 const initalState = { isOpen: false }
 
 export default function OrdersTable() {
   const [state, dispatch] = useReducer(OrderReducer, initalState)
-
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortBy, setSortBy] = useState('requested');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
   const [orders, setOrders] = useState([])
   const [orderDialogOpen, setOrderDailogOpen] = useState(false)
   const [selectedOrderId, setSelectedOrderId] = useState(0)
-  
+
   // Fetch orders on component mount
   useEffect(() => {
     fetchOrders();
   }, []);
-  
+
+  const OrderModalMemo = useMemo(() => {
+    if (orderDialogOpen) {
+      return <OrderModal orderId={selectedOrderId} isOpen={orderDialogOpen} handleClose={handleDialogClose} />;
+    }
+    return null;
+  }, [orderDialogOpen, selectedOrderId]);
+
+  const visibleRows = useMemo(
+    () =>
+      stableSort(orders, getComparator(sortOrder, sortBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [sortOrder, sortBy, page, rowsPerPage, orders],
+  );
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - orders.length) : 0;
+
   // GET all orders
   const fetchOrders = async () => {
     try {
@@ -36,6 +59,12 @@ export default function OrdersTable() {
     } catch (error) {
       console.error('Error fetching orders:', error)
     }
+  };
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = sortBy === property && sortOrder === 'asc';
+    setSortOrder(isAsc ? 'desc' : 'asc');
+    setSortBy(property);
   };
 
   const handleOrderClick = (event, id) => {
@@ -48,48 +77,64 @@ export default function OrdersTable() {
     setOrderDailogOpen(false)
   }
 
-  const OrderModalMemo = useMemo(() => {
-    if (orderDialogOpen) {
-      return <OrderModal orderId={selectedOrderId} isOpen={orderDialogOpen} handleClose={handleDialogClose} />;
-    }
-    return null;
-  }, [orderDialogOpen, selectedOrderId]);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  }
 
   return (
     <>
       <TableContainer className='orders-table-container' component={Paper}>
         <Table aria-label="simple table">
-          <TableHead className='orders-table-head'>
-            <TableRow>
-              <TableCell>Reference</TableCell>
-              <TableCell>Requested</TableCell>
-              <TableCell>Order</TableCell>
-              <TableCell>Ordered</TableCell>
-              <TableCell>Results Sent</TableCell>
-              <TableCell>Requested by</TableCell>
-            </TableRow>
-          </TableHead>
+          <SortingTableHead
+            order={sortOrder}
+            orderBy={sortBy}
+            onRequestSort={handleRequestSort}
+          />
           <TableBody className='orders-table-body'>
-            {orders.map((row) => (
+            {visibleRows.map((row) => (
               <TableRow
                 hover
                 key={row._id}
+                tabIndex={-1}
                 onClick={(event) => handleOrderClick(event, row._id)}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {row.ref}
                 </TableCell>
-                <TableCell>{row.requested ? dateFormatter(row.requested): ''}</TableCell>
+                <TableCell>{row.requested ? dateFormatter(row.requested) : ''}</TableCell>
                 <TableCell>{row.searchName}</TableCell>
                 <TableCell>{row.ordered ? dateFormatter(row.ordered) : ''}</TableCell>
                 <TableCell>{row.resultSent ? dateFormatter(row.resultSent) : ''}</TableCell>
                 <TableCell>{row.requestedBy}</TableCell>
               </TableRow>
             ))}
+            {emptyRows > 0 && (
+              <TableRow
+                style={{
+                  height: 53 * emptyRows,
+                }}
+              >
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[25, 50, 100]}
+        component="div"
+        count={orders.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
       {OrderModalMemo}
     </>
   );
---
+++ src/templates/headCells.js
@@ -0,0 +1,28 @@
+const headCells = [
+  {
+    id: 'ref',
+    label: 'Reference',
+  },
+  {
+    id: 'requested',
+    label: 'Requested on',
+  },
+  {
+    id: 'searchName',
+    label: 'Order',
+  },
+  {
+    id: 'ordered',
+    label: 'Ordered',
+  },
+  {
+    id: 'resultSent',
+    label: 'Results Sent',
+  },
+  {
+    id: 'requestedBy',
+    label: 'Requested by',
+  },
+];
+
+export { headCells };
